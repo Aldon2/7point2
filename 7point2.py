edaddeec3b74a0c2a0a80a352c7840b3569c7de5
Aldon Delport <aldonchad@yahoo.com>
2019-09-12 18:07:54 +1000

    Add files via upload

---
+++ 7point2.py
@@ -0,0 +1,19 @@
+#fname = input("Enter file name: ")
+fh = open("/home/aldon/Desktop/assignment 7.2/short.txt")
+count=(0)
+total=(0)
+for line in fh:
+    if line.startswith("X-DSPAM-Confidence:"): #continue
+        count=(count+1)
+        num=line[20:]
+        num=float(num)
+        total=(total+num)
+    #count=(count+1)
+        final=(total/count)
+        #print(num)
+#print(total)
+print(final)
+#print("Average spam confidence: ",final)
+#print(fh)
+#print(count)
+#print(num)
